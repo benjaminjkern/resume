9fe2e6567fb4a80b3fa41aa01e396c3bf36b1d78
benjaminjkern <benkern@pixelgate.net>
2025-12-11 15:09:44 -0800

    5+ years

--- src/sections/SummarySection.js
+++ src/sections/SummarySection.js
@@ -4,7 +4,7 @@ const SummarySection = () => {
     return (
         <Section title="Summary">
             <span>
-                Engineering leader with 7+ years managing teams and building
+                Engineering leader with 5+ years managing teams and building
                 scalable, customer-centric platforms in early-stage
                 environments. Co-founded 2 startups and led engineering at a
                 third. Experienced in hiring and mentoring engineers,
